024f72507d11f92ea899f0bea0e5444faa0fbddf
aaryap12 <140414954+aaryap12@users.noreply.github.com>
2025-04-20 23:31:02 +0530

    commit1

---
+++ Admin/js/script.js
@@ -0,0 +1,55 @@
+function loadPage(page) {
+  fetch(`${page}`)
+    .then((response) => response.text())
+    .then((data) => {
+      const container = document.getElementById("main-content");
+      container.innerHTML = data;
+
+      const scripts = container.querySelectorAll("script");
+      scripts.forEach((script) => {
+        const newScript = document.createElement("script");
+        if (script.src) {
+          newScript.src = script.src;
+        } else {
+          newScript.textContent = script.textContent;
+        }
+        document.body.appendChild(newScript);
+        script.remove();
+      });
+    })
+    .catch((err) => {
+      document.getElementById("main-content").innerHTML =
+        '<p class="text-danger">Failed to load content.</p>';
+      console.error(err);
+    });
+}
+
+document.getElementById("switchRoleBtn").addEventListener("click", function () {
+  const isAdmin = document
+    .getElementById("adminSidebar")
+    .classList.contains("d-none");
+
+  if (isAdmin) {
+    document.getElementById("adminSidebar").classList.remove("d-none");
+    document.getElementById("userSidebar").classList.add("d-none");
+    document.getElementById("cartIcon").classList.add("d-none");
+    document.getElementById("panelTitle").textContent = "📚Library Admin Panel";
+    document.getElementById("userRoleDisplay").textContent = "Admin";
+    document.getElementById("switchRoleBtn").textContent = "Switch to User";
+    loadPage("pages/dashboard.html");
+  } else {
+    document.getElementById("adminSidebar").classList.add("d-none");
+    document.getElementById("userSidebar").classList.remove("d-none");
+    document.getElementById("cartIcon").classList.remove("d-none");
+    document.getElementById("panelTitle").textContent = "📚Library User Portal";
+    document.getElementById("userRoleDisplay").textContent = "User";
+    document.getElementById("switchRoleBtn").classList.add("d-none");
+    loadPage("pages/user_dashboard.html");
+  }
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+  loadPage("pages/dashboard.html");
+});
+
+
